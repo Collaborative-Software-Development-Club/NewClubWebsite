e366bf19df902c562f4bb48d87cd62ca6f35ca0c
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-06-11 12:03:11 -0400

    added officers heading

--- src/components/MemberCard/index.tsx
+++ src/components/MemberCard/index.tsx
@@ -18,16 +18,24 @@ const MemberCard = (props: MemberCardProps) => {
     const shortenedDescription =
         props.description.substring(0, NUMBER_OF_CHARACTERS_IN_DESCRIPTION) + '...';
     return (
-        <Card width="300px">
+        <Card width="340px">
             <Padding all="md">
-                <FlexContainer direction="column" alignItems="center" gap="md">
-                    <MemberPhoto src={props.photo} />
-                    <FlexContainer direction="column" gap="sm" alignItems="start">
-                        <Heading3>{props.name}</Heading3>
-                        <Paragraph>{`Position: ${props.position}`}</Paragraph>
-                        {/* <Paragraph>{shortenedDescription}</Paragraph> */}
-                        <Button href={`/members/${props.name}`} variant="terciary">
-                            Read more
+                <FlexContainer direction="column" alignItems="stretch" gap="md">
+                    <FlexContainer direction="column" alignItems="center">
+                        <MemberPhoto src={props.photo} />
+                    </FlexContainer>
+                    <FlexContainer
+                        direction="column"
+                        justifyContent="space-between"
+                        alignItems="stretch"
+                    >
+                        <FlexContainer direction="column" gap="sm" alignItems="start">
+                            <Heading3>{props.name}</Heading3>
+                            <Paragraph>{`Position: ${props.position}`}</Paragraph>
+                            {/* <Paragraph>{shortenedDescription}</Paragraph> */}
+                        </FlexContainer>
+                        <Button href={`/members/${props.name}`} variant="primary">
+                            More
                         </Button>
                     </FlexContainer>
                 </FlexContainer>
--- src/layouts/MembersPage/index.tsx
+++ src/layouts/MembersPage/index.tsx
@@ -3,6 +3,7 @@ import MemberCard from '@/components/MemberCard';
 import Heading1 from '@/ui_library/components/Heading1';
 import Padding from '@/ui_library/components/Padding';
 import content from '@/websiteContent';
+import Heading2 from '@/ui_library/components/Heading2';
 
 const MembersPage = () => {
     return (
@@ -25,13 +26,16 @@ const MembersPage = () => {
                 </FlexContainer>
             </FlexContainer>
             */}
-            <FlexContainer direction="column" gap="none" alignItems="stretch">
+            <FlexContainer direction="column" gap="sm" alignItems="stretch">
                 <Padding vertical="none" horizontal="lg">
                     <Heading1>Our Members</Heading1>
                 </Padding>
+                <Padding vertical="none" horizontal="lg">
+                    <Heading2>Officers</Heading2>
+                </Padding>
                 <FlexContainer direction="column" fill>
                     <FlexContainer direction="row" fill scroll>
-                        <Padding all="lg">
+                        <Padding vertical="sm" horizontal="lg">
                             <FlexContainer direction="row" alignItems="stretch">
                                 {content.OFFICERS.map((member) => {
                                     return <MemberCard {...member} />;
