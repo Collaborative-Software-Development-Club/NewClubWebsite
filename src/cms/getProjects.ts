58beb722d3f1e67ae1e5d021c701192289f82e5e
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-06-26 21:29:13 -0400

    refactored database fetcher

--- src/cms/getProjects.ts
+++ src/cms/getProjects.ts
@@ -1,69 +1,28 @@
 import { Client } from '@notionhq/client';
 import assert from 'assert';
-import NotionDatabaseFetcher, { ComparableData } from './NotionDatabaseFetcher';
-import {
-    NotionDateParser,
-    NotionMultiSelectParser,
-    NotionRichTextParser,
-    NotionTitleParser,
-    NotionURLParser,
-} from './parsing';
-import DataSortedByDescendingDate from './DataSortedByDescendingDate';
+import { NotionDatabaseFetcher } from './fetching';
+import compareNotionDatesDescending from './fetching/compareNotionDatesDescending';
 
 export default async function getProjects(): Promise<ProjectData[]> {
     const notion = new Client({ auth: process.env.NOTION_API_KEY });
     const databaseId = process.env.PROJECTS_DATABASE_ID;
     assert(databaseId !== undefined, "PROJECTS_DATABASE_ID can't be undefined");
-    return new NotionProjectsFetcher(databaseId, notion).get();
+    const databaseFetcher = new NotionDatabaseFetcher<ProjectData>(databaseId, notion, mapping);
+    return (await databaseFetcher.get()).sort(compareNotionDatesDescending);
 }
 
-export class ProjectData extends DataSortedByDescendingDate {
-    constructor(
-        public title: string,
-        public description: string,
-        public tags: string[],
-        public url: string,
-        public date: Date | [Date, Date],
-    ) {
-        super();
-    }
+export interface ProjectData {
+    title: string;
+    description: string;
+    tags: string[];
+    url: string;
+    date: Date | [Date, Date];
 }
 
-const DATABASE_PROPERTIES = {
-    name: 'Name',
+const mapping = {
+    title: 'Name',
     description: 'Description',
     tags: 'Tags',
-    link: 'Link',
+    url: 'Link',
     date: 'Timeline',
 };
-
-class NotionProjectsFetcher extends NotionDatabaseFetcher<ProjectData> {
-    constructor(databaseId: string, notionClient: Client) {
-        super(databaseId, notionClient);
-    }
-    protected parsePageProperties(properties: any): ProjectData {
-        const parsedProjectName = new NotionTitleParser(
-            properties[DATABASE_PROPERTIES.name],
-        ).parse();
-        this.assertPropertyIsMissing(parsedProjectName, DATABASE_PROPERTIES.name);
-        const parsedDescription = new NotionRichTextParser(
-            properties[DATABASE_PROPERTIES.description],
-        ).parse();
-        this.assertPropertyIsMissing(parsedDescription, DATABASE_PROPERTIES.description);
-        const parsedTags = new NotionMultiSelectParser(
-            properties[DATABASE_PROPERTIES.tags],
-        ).parse();
-        this.assertPropertyIsMissing(parsedTags, DATABASE_PROPERTIES.tags);
-        const parsedLink = new NotionURLParser(properties[DATABASE_PROPERTIES.link]).parse();
-        this.assertPropertyIsMissing(parsedLink, DATABASE_PROPERTIES.link);
-        const parsedDate = new NotionDateParser(properties[DATABASE_PROPERTIES.date]).parse();
-        this.assertPropertyIsMissing(parsedDate, DATABASE_PROPERTIES.date);
-        return new ProjectData(
-            parsedProjectName,
-            parsedDescription,
-            parsedTags,
-            parsedLink,
-            parsedDate,
-        );
-    }
-}
